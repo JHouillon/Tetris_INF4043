b232859325942a3b999439450d0704384f6c215c
Piguom <pfrin44@gmail.com>
2016-03-14 19:14:48 +0100

    Plateau

--- Projet/Tetris_INF4043/src/jeu/Plateau.java
+++ Projet/Tetris_INF4043/src/jeu/Plateau.java
@@ -1,37 +1,68 @@
 package jeu;
 
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+import DAO.CsvRW;
+import Pieces.Pieces;
+import Plateau.Menu;
 import users.Joueur;
 
 public class Plateau
 {
-	private int dimension;
-	private int difficulte;
 	
-	Joueur j1;
+	Pieces [] piecesTab;
+//	static Zone zone = new Zone();
+	
+	int nbCaseTotalTab=100;
+	
+	
 
-	public Plateau(int dimension, int difficulte)
-	{
-		this.dimension = dimension;
-		this.difficulte = difficulte;
+	public Plateau(String link) {
+		
+		System.out.println("Création PLateau");
+		
+		CsvRW ReaderCsv=new CsvRW(link);
+		
+		
+		Pieces[] tab = ReaderCsv.findAllPieces();
+		
+		this.piecesTab = tab;
 	}
 
-	public int getDimension()
-	{
-		return dimension;
-	}
+	private static void ConfigurationFenetre() {
+		JFrame fenetre = new JFrame();
+		
+		Menu menu = new Menu();
+		
+		//JPanel pan = new JPanel();
+		//JPanel pan1 = new JPanel();
+		
+		//JTextField text = new JTextField("Maxime a un petit zizi !");
+		//JTextField text1 = new JTextField("Daniel à le sida !");
+		
+		fenetre.setSize(1024, 720);
+		fenetre.setTitle("Eternity");		
 
-	public void setDimension(int dimension)
-	{
-		this.dimension = dimension;
+		//JLabel im = new JLabel( new ImageIcon( "src/main/java/Pieces/ESIEA_2014_logo.jpg"));
+		
+		//pan.setBackground(Color.RED);
+		//pan.add(text);
+		//pan.add(im);
+		
+		//pan1.setBackground(Color.ORANGE);
+		//pan1.add(text1);
+		
+		fenetre.setJMenuBar(menu);
+		//fenetre.getContentPane().add(pan, BorderLayout.EAST);
+		//fenetre.getContentPane().add(pan1, BorderLayout.WEST);
+		//fenetre.getContentPane().add(zone);
+		fenetre.setLayout(new GridLayout(1,2));
+		fenetre.setVisible(true);
 	}
-
-	public int getDifficulte()
-	{
-		return difficulte;
-	}
-
-	public void setDifficulte(int difficulte)
-	{
-		this.difficulte = difficulte;
+	
+	public static void main(String[] args){
+		ConfigurationFenetre();
 	}
 }
