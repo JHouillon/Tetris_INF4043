b9ad21f80e58167abab606d64bcdb5d1d9dacbd9
Piguom <pfrin44@gmail.com>
2016-03-15 13:05:12 +0100

    Changes cause of conflits

---
+++ Projet/Tetris_INF4043/src/jeu/Plateau.java
@@ -0,0 +1,229 @@
+package jeu;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import pions.Piece;
+import pions.Forme.Formes;
+public class Plateau extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	final int BoardWidth = 10;
+	final int BoardHeight = 22;
+	Timer timer;
+	boolean isFallingFinished = false;
+	boolean isStarted = false;
+	boolean isPaused = false;
+	int numLinesRemoved = 0;
+	int curX = 0;
+	int curY = 0;
+	JLabel statusbar;
+	Piece curPiece;
+	Formes[] f;
+	public Plateau(ModeDeJeu parent) {
+		setFocusable(true);
+		curPiece = new Piece();
+		timer = new Timer(400, this);
+		timer.start();
+		statusbar = parent.getStatusBar();
+		f = new Formes[BoardWidth * BoardHeight];
+		addKeyListener(new TAdapter());
+		clearBoard();
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (isFallingFinished) {
+			isFallingFinished = false;
+			newPiece();
+		} else {
+			oneLineDown();
+		}
+	}
+	int squareWidth() {
+		return (int) getSize().getWidth() / BoardWidth;
+	}
+	int squareHeight() {
+		return (int) getSize().getHeight() / BoardHeight;
+	}
+	Formes shapeAt(int x, int y) {
+		return f[(y * BoardWidth) + x];
+	}
+	public void start() {
+		if (isPaused)
+			return;
+		isStarted = true;
+		isFallingFinished = false;
+		numLinesRemoved = 0;
+		clearBoard();
+		newPiece();
+		timer.start();
+	}
+	private void pause() {
+		if (!isStarted)
+			return;
+		isPaused = !isPaused;
+		if (isPaused) {
+			timer.stop();
+			statusbar.setText("paused");
+		} else {
+			timer.start();
+			statusbar.setText(String.valueOf(numLinesRemoved));
+		}
+		repaint();
+	}
+	public void paint(Graphics g) {
+		super.paint(g);
+		Dimension size = getSize();
+		int boardTop = (int) size.getHeight() - BoardHeight * squareHeight();
+		for (int i = 0; i < BoardHeight; ++i) {
+			for (int j = 0; j < BoardWidth; ++j) {
+				Formes f = shapeAt(j, BoardHeight - i - 1);
+				if (f != Formes.Rien)
+					drawSquare(g, 0 + j * squareWidth(), boardTop + i * squareHeight(), f);
+			}
+		}
+		if (curPiece.getForme() != Formes.Rien) {
+			for (int i = 0; i < 4; ++i) {
+				int x = curX + curPiece.x(i);
+				int y = curY - curPiece.y(i);
+				drawSquare(g, 0 + x * squareWidth(), boardTop + (BoardHeight - y - 1) * squareHeight(),
+						curPiece.getForme());
+			}
+		}
+	}
+	private void dropDown() {
+		int newY = curY;
+		while (newY > 0) {
+			if (!tryMove(curPiece, curX, newY - 1))
+				break;
+			--newY;
+		}
+		pieceDropped();
+	}
+	private void oneLineDown() {
+		if (!tryMove(curPiece, curX, curY - 1))
+			pieceDropped();
+	}
+	private void clearBoard() {
+		for (int i = 0; i < BoardHeight * BoardWidth; ++i)
+			f[i] = Formes.Rien;
+	}
+	private void pieceDropped() {
+		for (int i = 0; i < 4; ++i) {
+			int x = curX + curPiece.x(i);
+			int y = curY - curPiece.y(i);
+			f[(y * BoardWidth) + x] = curPiece.getForme();
+		}
+		removeFullLines();
+		if (!isFallingFinished)
+			newPiece();
+	}
+	private void newPiece() {
+		curPiece.setRandomForme();
+		System.out.println(curPiece.getForme());
+		curX = BoardWidth / 2 + 1;
+		curY = BoardHeight - 1 + curPiece.minY();
+		if (!tryMove(curPiece, curX, curY)) {
+			curPiece.setForme(Formes.Rien);
+			timer.stop();
+			isStarted = false;
+			statusbar.setText("Perdu");
+		}
+	}
+	private boolean tryMove(Piece newPiece, int newX, int newY) {
+		for (int i = 0; i < 4; ++i) {
+			int x = newX + newPiece.x(i);
+			int y = newY - newPiece.y(i);
+			if (x < 0 || x >= BoardWidth || y < 0 || y >= BoardHeight)
+				return false;
+			if (shapeAt(x, y) != Formes.Rien)
+				return false;
+		}
+		curPiece = newPiece;
+		curX = newX;
+		curY = newY;
+		repaint();
+		return true;
+	}
+	private void removeFullLines() {
+		int numFullLines = 0;
+		for (int i = BoardHeight - 1; i >= 0; --i) {
+			boolean lineIsFull = true;
+			for (int j = 0; j < BoardWidth; ++j) {
+				if (shapeAt(j, i) == Formes.Rien) {
+					lineIsFull = false;
+					break;
+				}
+			}
+			if (lineIsFull) {
+				++numFullLines;
+				for (int k = i; k < BoardHeight - 1; ++k) {
+					for (int j = 0; j < BoardWidth; ++j)
+						f[(k * BoardWidth) + j] = shapeAt(j, k + 1);
+				}
+			}
+		}
+		if (numFullLines > 0) {
+			numLinesRemoved += numFullLines;
+			statusbar.setText(String.valueOf(numLinesRemoved));
+			isFallingFinished = true;
+			curPiece.setForme(Formes.Rien);
+			repaint();
+		}
+	}
+	private void drawSquare(Graphics g, int x, int y, Formes f) {
+		Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102), new Color(102, 204, 102),
+				new Color(102, 102, 204), new Color(204, 204, 102), new Color(204, 102, 204), new Color(102, 204, 204),
+				new Color(218, 170, 0) };
+		Color color = colors[f.ordinal()];
+		g.setColor(color);
+		g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+		g.setColor(color.brighter());
+		g.drawLine(x, y + squareHeight() - 1, x, y);
+		g.drawLine(x, y, x + squareWidth() - 1, y);
+		g.setColor(color.darker());
+		g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1);
+		g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1);
+	}
+	class TAdapter extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			if (!isStarted || curPiece.getForme() == Formes.Rien) {
+				return;
+			}
+			int keycode = e.getKeyCode();
+			if (keycode == 'p' || keycode == 'P') {
+				pause();
+				return;
+			}
+			if (isPaused)
+				return;
+			switch (keycode) {
+			case KeyEvent.VK_LEFT:
+				tryMove(curPiece, curX - 1, curY);
+				break;
+			case KeyEvent.VK_RIGHT:
+				tryMove(curPiece, curX + 1, curY);
+				break;
+			case KeyEvent.VK_DOWN:
+				tryMove(curPiece.rotateRight(), curX, curY);
+				break;
+			case KeyEvent.VK_UP:
+				tryMove(curPiece.rotateLeft(), curX, curY);
+				break;
+			case KeyEvent.VK_SPACE:
+				dropDown();
+				break;
+			case 'd':
+				oneLineDown();
+				break;
+			case 'D':
+				oneLineDown();
+				break;
+			}
+		}
+	}
+}
