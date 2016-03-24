d3f34e5148592e53e955edbc7590def64bcc85fc
Piguom <pfrin44@gmail.com>
2016-03-24 13:36:33 +0100

    Background image + high score

--- Projet/Tetris_INF4043/src/jeu/PageLancement.java
+++ Projet/Tetris_INF4043/src/jeu/PageLancement.java
@@ -1,12 +1,17 @@
 package jeu;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,20 +26,26 @@ public class PageLancement {
 	static JPanel pan1 = new JPanel();
 	static JPanel pan2 = new JPanel();
 	static JPanel pan3 = new JPanel();
-	static JLabel tetris = new JLabel(new ImageIcon("src/jeu/tetris.jpg"));
 	final static JTextField name = new JTextField("Entrez votre nom");
 	static JButton jouer = new JButton("Jouer");
 	static JButton commande = new JButton("Régles / Commandes");
 	static JButton valider = new JButton("Solo");
 	static JButton multi = new JButton("Multi");
 	static String nom;
+	static JPanel panel = new JPanel(new BorderLayout());
+	static JLabel score = new JLabel("High Score: 0");
 	
-	public static void Lancement(){
+	public static void Lancement() throws IOException{
 		
 		lancement.setTitle("Tetris");
 		lancement.setSize(500,400);
-		lancement.setResizable(false);
+		lancement.setResizable(false);	
 		lancement.setLocationRelativeTo(null);
+		
+
+		panel = setBackgroundImage(lancement, new File("src/jeu/tetris.jpg"));
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(500,400));
 		
 		name.addFocusListener(new FocusAdapter() {
 		    public void focusGained(FocusEvent e) {
@@ -43,14 +54,24 @@ public class PageLancement {
 		});
 		
 		pan.add(name);
+		pan.setOpaque(false);
 		pan1.add(valider);
+		pan1.setOpaque(false);
 		pan2.add(multi);
+		pan2.setOpaque(false);
+		pan3.add(score);
+		pan3.setOpaque(false);
 		
-		lancement.add(pan,BorderLayout.NORTH);
-		lancement.add(pan1,BorderLayout.WEST);
-		lancement.add(pan2,BorderLayout.EAST);
-		lancement.add(pan3,BorderLayout.SOUTH);
-		lancement.add(tetris,BorderLayout.CENTER);
+		panel.add(pan);
+		panel.add(pan1);
+		panel.add(pan2);
+		panel.add(pan3);
+		
+		panel.getComponent(0).setBounds(200,5,100,30);
+		panel.getComponent(1).setBounds(20,270,58,30);
+		panel.getComponent(2).setBounds(420,270,58,30);
+		panel.getComponent(3).setBounds(200,375,100,100);
+		lancement.pack();
 		lancement.setVisible(true);
 	}
 	
@@ -59,7 +80,29 @@ public class PageLancement {
 		return nom;
 	}
 	
-	public static void main(String[] args)
+	public static JPanel setBackgroundImage(JFrame frame, final File img) throws IOException
+	{
+		JPanel panel = new JPanel()
+		{
+			private static final long serialVersionUID = 1;
+			
+			private BufferedImage buf = ImageIO.read(img);
+			
+			@Override
+			protected void paintComponent(Graphics g)
+			{
+				super.paintComponent(g);
+				g.drawImage(buf, 0,0, null);
+			}
+		};
+		
+		frame.setContentPane(panel);
+		
+		return panel;
+	}
+
+	
+	public static void main(String[] args) throws IOException
 	{
 		Lancement();
 		
